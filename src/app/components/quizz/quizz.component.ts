d4d2f33d1da3dea0c44e994419bc647fa2f943c1
Davi Samuel <davisamasoa@gmail.com>
2023-05-25 18:01:25 -0300

    project finish

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,78 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-import quizz_questions from "../../../assets/data/quizz_questions.json"
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
-
 export class QuizzComponent implements OnInit {
+  title: string = '';
+  img: string = '';
+  questions: any;
+  questionSelected: any;
 
-  title:string = ""
+  answers: string[] = [];
+  answerSelected: string = '';
 
-  questions:any
-  questionSelected:any
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  answers:string[] = []
-  answerSelected:string =""
-
-  questionIndex:number =0
-  questionMaxIndex:number=0
-
-  finished:boolean = false
-
-  constructor() { }
+  finished: boolean = false;
+  bgImg: any = '';
+  constructor() {}
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.img = quizz_questions.img;
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+      this.bgImg = {
+        'background-image': `url(${quizz_questions.img})`,
+      };
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
 
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
-    }
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
 
-  }
-
-  playerChoose(value:string){
-    this.answers.push(value)
-    this.nextStep()
-
-  }
-
-  async nextStep(){
-    this.questionIndex+=1
-
-    if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
-      this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+      console.log(this.questionIndex);
+      console.log(this.questionMaxIndex);
     }
   }
 
-  async checkResult(anwsers:string[]){
-
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
-    })
-
-    return result
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
   }
 
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+    }
+  }
+
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
 }
